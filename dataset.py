033ce99c1e3da950b4bbaf7cbb1b3ed7959ad2e6
Arttu Pönni <arttuponni@gmail.com>
2021-07-01 09:53:07 +0300

    Initial working complex version.

--- dataset.py
+++ dataset.py
@@ -1,88 +1,162 @@
 import numpy.random as npr
 import numpy as np
+from scipy import real, imag
 from scipy.integrate import quad
-from scipy.interpolate import interp1d
 from torch.utils.data.dataset import Dataset
 import torch
+from constants import dreal, dcomplex
 
 
 class AdSBHDataset(Dataset):
     def __init__(self, N=1000, threshold=0.01):
-        self.size = 2*N
+        self.size = N
         self.threshold = threshold
-        self.L, self.V, self.labels = self.gen_data()
+        self.L, self.zs, self.V = self.gen_data()
 
     def __getitem__(self, index):
-        return self.L[index], self.V[index], self.labels[index]
+        return self.L[index], self.V[index]
 
     def __len__(self):
         return len(self.L)
 
     def gen_data(self):
         N = self.size
-        # Generate the curve
-        L_list = []
-        V_list = []
-        for zs in np.linspace(0.1, 0.999, num=100):
-            L = self.integrate_L(zs)
-            V = self.integrate_V(zs)
-            if V < 0:
-                L_list.append(L)
-                V_list.append(V)
+        zs_max, L_max = self.get_L_max()
+        L_data = []
+        zs_data = []
+        V_data = []
+        for _ in range(N):
+            L = npr.uniform(0.01, 2)
+            if L <= L_max:
+                # Binary search
+                zs = self.find_zs_binary(L, zs_max)
             else:
-                break
-        curve = interp1d(L_list, V_list)
-        # Randomly draw datapoints
-        N_pos = 0
-        N_neg = 0
-        pos_data = np.empty((N, 2))
-        neg_data = np.empty((N, 2))
-        while N_neg < N:
-            L_val = npr.uniform(min(L_list), max(L_list))
-            V_val = npr.uniform(min(V_list), max(V_list))
-            if abs(V_val-curve(L_val)) > self.threshold:
-                neg_data[N_neg] = np.array([L_val, V_val])
-                N_neg += 1
-        while N_pos < N:
-            L_val = npr.uniform(min(L_list), max(L_list))
-            pos_data[N_pos] = np.array(
-                [L_val, curve(L_val)
-                 + npr.uniform(-self.threshold, self.threshold)])
-            N_pos += 1
+                # Newton's method
+                # TODO: Add a smarter init for Newton
+                zs = self.find_zs_newton(L, zs_max + 0.1j)
+            V = self.integrate_V(zs) + npr.uniform(-self.threshold, self.threshold)
+            L_data.append(L)
+            zs_data.append(zs)
+            V_data.append(V)
+        L_data = torch.tensor(L_data, dtype=dreal)
+        zs_data = torch.tensor(zs_data, dtype=dcomplex)
+        V_data = torch.tensor(V_data, dtype=dcomplex)
+        return L_data, zs_data, V_data
 
-        # Shuffle the data
-        perm = torch.randperm(2*N)
-        pos = torch.tensor(pos_data, dtype=torch.double)
-        neg = torch.tensor(neg_data, dtype=torch.double)
-        xs = torch.cat([pos, neg])[perm]
-        ys = torch.cat([torch.zeros(N, dtype=torch.double),
-                        torch.ones(N, dtype=torch.double)])[perm]
-        return xs[:, 0], xs[:, 1], ys
+    def find_zs_binary(self, L, zs_max):
+        '''
+        Intended for real zs.
+        '''
+        zs_low = 0.001
+        zs_high = zs_max
+        while zs_high - zs_low > 1e-8:
+            zs_mid = (zs_high + zs_low) / 2
+            L_mid = self.integrate_L(zs_mid)
+            assert np.abs(L_mid.imag) < 1e-8
+            L_mid = L_mid.real
+            if L_mid < L:
+                zs_low = zs_mid
+            else:
+                zs_high = zs_mid
+        zs_mid = (zs_high + zs_low) / 2
+        return zs_mid
+
+    def find_zs_newton(self, L, init, max_steps=10):
+        zs = init
+        for i in range(max_steps):
+            zs -= (self.integrate_L(zs) - L) / self.integrate_dL(zs)
+            if np.abs(self.integrate_L(zs) - L) < 1e-8:
+                break
+        return zs
+
+    def get_L_max(self):
+        '''
+        Returns the point where L is maximal such that
+        zs is still real. This is the last point on the
+        real axis along the real L curve.
+        '''
+        zs_UV, zs_IR = 0.001, 0.999
+        dL_IR = self.integrate_dL(zs_IR).real
+        dL_UV = self.integrate_dL(zs_UV).real
+        assert dL_IR < 0 and dL_UV > 0
+        while zs_IR - zs_UV > 1e-8:
+            zs_mid = (zs_UV + zs_IR) / 2
+            dL_mid = self.integrate_dL(zs_mid).real
+            if dL_mid < 0:
+                zs_IR = zs_mid
+            else:
+                zs_UV = zs_mid
+        zs_mid = (zs_UV + zs_IR) / 2
+        L_max = self.integrate_L(zs_mid)
+        assert np.abs(L_max.imag) < 1e-8
+        return zs_mid, L_max.real
 
     def eval_f(self, z):
-        return 1-z**4
+        return (1 - z) * (1 + z) * (1 + z**2)
+
+    def eval_df(self, z):
+        return -4 * z**3
 
     def eval_g(self, z):
-        return 1/(1-z**4)
+        return 1 / ((1 - z) * (1 + z) * (1 + z**2))
 
     def integrate_L(self, zs):
         def integrand(y):
-            z = zs*(1-y)*(1+y)
-            return np.sqrt(self.eval_g(z))*y/np.sqrt(
-                self.eval_f(z)/((1-y)**4*(1+y)**4*self.eval_f(zs))-1)
-        L = 4*zs*quad(integrand, 0.0, 1.0)[0]
+            z = zs * (1 - y) * (1 + y)
+            return np.sqrt(self.eval_g(z)) * y / np.sqrt(
+                self.eval_f(z) / ((1 - y)**4 * (1 + y)**4 * self.eval_f(zs)) - 1)
+
+        def integrand_real(y):
+            return real(integrand(y))
+
+        def integrand_imag(y):
+            return imag(integrand(y))
+
+        L = 4 * zs * (quad(integrand_real, 0, 1)[0] + 1j * quad(integrand_imag, 0, 1)[0])
         return L
+
+    def integrate_dL(self, zs):
+        def integrand(y):
+            # NOTE: This only holds for AdS
+            return 4 * (1 - y)**2 * (1 + y)**2 * (1 - 3 * zs**4 + (1 - y)**4 * (1 + y)**4 * zs**4 * (1 + zs**4)) / \
+                (np.sqrt((1 - zs) * (1 + zs) * (1 + zs**2)) * np.sqrt(4 - 6 * y**2 + 4 * y**4 - y**6) *
+                    (1 - (1 - y)**4 * (1 + y)**4 * zs**4)**1.5)
+
+        def integrand_real(y):
+            return real(integrand(y))
+
+        def integrand_imag(y):
+            return imag(integrand(y))
+
+        dL = quad(integrand_real, 0, 1)[0] + 1j * quad(integrand_imag, 0, 1)[0]
+        return dL
 
     def integrate_V(self, zs):
         def integrand(y):
-            z = zs*(1-y)*(1+y)
-            return np.sqrt(self.eval_f(z)*self.eval_g(z))/((1-y)**2*(1+y)**2)*y*(
-                1/np.sqrt(1-((1-y)**4*(1+y)**4*self.eval_f(zs))/self.eval_f(z))
+            z = zs * (1 - y) * (1 + y)
+            return np.sqrt(self.eval_f(z) * self.eval_g(z)) / ((1 - y)**2 * (1 + y)**2) * y * (
+                1 / np.sqrt(1 - ((1 - y)**4 * (1 + y)**4 * self.eval_f(zs)) / self.eval_f(z))
                 - 1)
 
-        def disconnected(z):
-            return np.sqrt(self.eval_f(z)*self.eval_g(z))/z**2
+        def integrand_real(y):
+            return real(integrand(y))
 
-        V = 4/zs*quad(integrand, 0.0, 1.0)[0]
-        V -= 2*quad(disconnected, zs, 1)[0]
+        def integrand_imag(y):
+            return imag(integrand(y))
+
+        def disconnected(y):
+            z = 1 - (1 - zs) * y
+            # Simplified by noting that in AdS, f(z)*g(z) = 1.
+            # NOTE: Only works for AdS.
+            # return np.sqrt(self.eval_f(z) * self.eval_g(z)) / z**2
+            return 1 / z**2
+
+        def disconnected_real(y):
+            return real(disconnected(y))
+
+        def disconnected_imag(y):
+            return imag(disconnected(y))
+
+        V = 4 / zs * (quad(integrand_real, 0, 1)[0] + 1j * quad(integrand_imag, 0, 1)[0])
+        V -= 2 * (1 - zs) * (quad(disconnected_real, 0, 1)[0] + 1j * quad(disconnected_imag, 0, 1)[0])
         return V
--- model.py
+++ model.py
@@ -1,13 +1,18 @@
+import logging
 import torch
 import torch.nn as nn
-from constants import dtype
+import numpy as np
+from scipy.interpolate import interp1d
+from constants import dreal, dcomplex
 
 
 class AdSBHNet(nn.Module):
-    def __init__(self, N=5):
+    def __init__(self, N=5, std=1.0):
         super(AdSBHNet, self).__init__()
-        self.a = nn.Parameter(torch.normal(0.0, 0.5, size=(N,), dtype=dreal))
-        self.b = nn.Parameter(torch.normal(0.0, 0.5, size=(N,), dtype=dreal))
+        self.a = nn.Parameter(torch.normal(0.0, std, size=(N,), dtype=dreal))
+        self.b = nn.Parameter(torch.normal(0.0, std, size=(N,), dtype=dreal))
+        self.curve_L = []
+        self.curvs_zs = []
 
     def forward(self, Ls):
         '''
@@ -15,59 +20,143 @@ class AdSBHNet(nn.Module):
         instead of torchdiffeq.
         '''
         V = torch.zeros_like(Ls, dtype=dcomplex)
-        zs_prev = 0.0009
-        L_prev = self.integrate_L(zs_prev)
-        '''
-        This part could be made faster and more accurate if
-        we first find L_max and then binary search for zs.
-        This assumes that in (0, L_max) the function
-        zs = zs(L) is monotonous.
-        '''
-        for zs in torch.linspace(0.001, 0.999, 100):
-            if self.integrate_V(zs) > 0:
-                break
-            L = self.integrate_L(zs)
-            for i in range(len(Ls)):
-                if L_prev < Ls[i] < L:
-                    slope = (zs-zs_prev)/(L-L_prev)
-                    V[i] = self.integrate_V(slope*(Ls[i]-L_prev)+zs_prev)
-            zs_prev = zs
-            L_prev = L
+
+        self.find_curve()
+        curve = interp1d(self.curve_L, self.curve_zs)
+        zs_max, L_max = self.get_L_max()
+
+        for i, L in enumerate(Ls):
+            init = complex(curve(L.item()))
+            _zs = self.find_zs_newton(L, init)
+            # TODO: Verify that flipping possibly negative imaginary parts is always safe
+            zs = torch.complex(_zs.real, _zs.imag.abs())
+            if zs.real < 0:
+                logging.error(f'Real part of zs is negative: {zs} for L = {L}')
+                assert False, 'Real part of zs is negative.'
+            V[i] = self.integrate_V(zs)
+            assert not torch.isnan(V[i])
         return V
 
-    def get_Lmax(self):
+    def find_curve(self):
+        zs_max, L_max = self.get_L_max()
+        self.curve_L = [0.0, L_max.item()]
+        self.curve_zs = [complex(0.0, 0.0), zs_max.item()]
+
+        for L in np.linspace(L_max.item() + 0.01, 2, 50):
+            if np.abs(self.curve_zs[-1].imag) < 1e-8:
+                init = self.curve_zs[-1] + 0.1j
+            else:
+                init = self.curve_zs[-1]
+            _zs = self.find_zs_newton(L, init)
+            zs = complex(_zs.real.item(), _zs.imag.abs().item() if L > L_max else 0.0)
+            self.curve_zs.append(zs)
+            self.curve_L.append(L)
+        for i in range(2, len(self.curve_zs)):
+            diff = np.abs(self.curve_zs[i] - self.curve_zs[i - 1])
+            if diff > 0.1:
+                logging.error(f'Generated (L, zs) -curve looks discontinuous: |z[i] - z[i-1]| = {diff}')
+                assert False, '(zs, L) -curve is discontinuous.'
+
+    def find_zs_newton(self, L, init, max_steps=25, retry=True):
+        if not isinstance(init, torch.Tensor) or init.dtype != dcomplex:
+            zs = torch.as_tensor(init, dtype=dcomplex)
+        zs = [init]
+        _L = self.integrate_L(zs[-1])
+        for i in range(max_steps):
+            dL = self.integrate_dL(zs[-1])
+            zs.append(zs[-1] - (_L - L) / dL)
+            if zs[-1].abs() > 100 or torch.isnan(zs[-1]):
+                logging.error(f'Something wrong in Newton:\n\tzs = {[_zs.item() for _zs in zs]}\n\tL = {L}\n\t_L = {_L}\n\tdL = {dL}')
+            _L = self.integrate_L(zs[-1])
+            diff = torch.abs(_L - L)
+            if diff < 1e-8:
+                return zs[-1]
+        if retry:
+            logging.warning(f'Newton\'s method failed to converge in {max_steps} iterations for L = {L}\n\tzs = {zs[-1]}\n\tdiff = {diff}\n\tinit = {init}. Retrying with default init 0.5+0.5j.')
+            return self.find_zs_newton(L, 0.5 + 0.5j, retry=False)
+        else:
+            logging.error(f'Newton\'s method failed to converge in {max_steps} iterations for L = {L}\n\tzs = {zs[-1]}\n\tdiff = {diff}\n\tinit = {init}.')
+            return zs[-1]
+
+    def get_L_max(self):
+        '''
+        Returns the point where L is maximal such that
+        zs is still real. This is the last point on the
+        real axis along the real L curve.
+        '''
         zs_UV, zs_IR = 0.001, 0.999
-        V_IR = self.integrate_V(zs_IR)
-        V_UV = self.integrate_V(zs_UV)
-        assert V_IR > 0 and V_UV < 0
-        while zs_IR-zs_UV > 1e-8:
-            zs_mid = (zs_UV+zs_IR)/2
-            V_mid = self.integrate_V(zs_mid)
-            if V_mid > 0:
+        dL_IR = self.integrate_dL(zs_IR).real
+        dL_UV = self.integrate_dL(zs_UV).real
+        assert dL_IR < 0 and dL_UV > 0
+        while zs_IR - zs_UV > 1e-8:
+            zs_mid = (zs_UV + zs_IR) / 2
+            dL_mid = self.integrate_dL(zs_mid).real
+            if dL_mid < 0:
                 zs_IR = zs_mid
-                V_IR = V_mid
             else:
                 zs_UV = zs_mid
-                V_UV = V_mid
-        return self.integrate_L((zs_UV+zs_IR)/2)
+        zs_mid = (zs_UV + zs_IR) / 2
+        L_max = self.integrate_L(zs_mid)
+        assert L_max.imag.abs() < 1e-8
+        return torch.tensor(zs_mid, dtype=dcomplex), L_max.real
 
     def integrate_L(self, zs):
-        zs = zs if isinstance(zs, torch.Tensor) else torch.tensor(zs, dtype=dtype)
-        y = torch.linspace(0.001, 0.999, steps=1000, dtype=dtype)
-        z = zs*(1-y)*(1+y)
-        f = self.eval_f(z)
+        zs = zs if isinstance(zs, torch.Tensor) else torch.as_tensor(
+            zs, dtype=dcomplex)
+        y = torch.linspace(0.001, 0.999, steps=1000, dtype=dreal)
+        z = zs * (1 - y) * (1 + y)
         sqrtg = self.eval_g(z).sqrt()
-        fs = self.eval_f(zs.unsqueeze(-1))
-        integrand = sqrtg/torch.sqrt(f/(fs*(1-y)**4*(1+y)**4)-1)*y
+        f_over_fs = torch.exp(self.eval_a(zs) - self.eval_a(z)) * (1 - z) * (1 + z) * (1 + z**2) \
+            / ((1 - zs) * (1 + zs) * (1 + zs**2))
+        integrand = sqrtg / \
+            torch.sqrt(f_over_fs / ((1 - y)**4 * (1 + y)**4) - 1) * y
         # We extrapolate to y=0
-        y = torch.cat((torch.tensor([0.0], dtype=dtype), y))
-        integrand = torch.cat((((integrand[1]-integrand[0])/(y[1]-y[0])*(-y[0])+integrand[0]).unsqueeze(-1), integrand))
+        y = torch.cat((torch.tensor([0.0], dtype=dreal), y))
+        integrand = torch.cat(
+            (((integrand[1] - integrand[0]) / (y[1] - y[0]) * (-y[0]) + integrand[0]).unsqueeze(-1), integrand))
         # Add analytically known value at y=1
-        y = torch.cat((y, torch.tensor([1.0], dtype=dtype)))
-        integrand = torch.cat((integrand, torch.tensor([0.0], dtype=dtype)))
+        y = torch.cat((y, torch.tensor([1.0], dtype=dreal)))
+        integrand = torch.cat((integrand, torch.tensor([0.0], dtype=dcomplex)))
         # Integrate
-        L = 4*zs*torch.trapz(integrand, y)
+        L = 4 * zs * torch.trapz(integrand, y)
+        if torch.isnan(L):
+            logging.error(f'integrate_L({zs}) = {L} for a = {self.a} b = {self.b}')
+            logging.error(f'integrand is {integrand}')
+            assert False, 'L-integrand is nan.'
         return L
+
+    def integrate_dL(self, zs):
+        zs = zs if isinstance(zs, torch.Tensor) else torch.as_tensor(
+            zs, dtype=dcomplex)
+        y = torch.linspace(0.001, 0.999, steps=1000, dtype=dreal)
+        z = zs * (1 - y) * (1 + y)
+        f = self.eval_f(z)
+        fs = self.eval_f(zs.unsqueeze(-1))
+        g = self.eval_g(z)
+        dlogf = -4 * z**3 / ((1 - z) * (1 + z) * (1 + z**2)) - self.eval_da(z)
+        dlogfs = -4 * zs**3 / ((1 - zs) * (1 + zs) * (1 + zs**2)) - self.eval_da(zs)
+        dlogg = 4 * z**3 / ((1 - z) * (1 + z) * (1 + z**2)) + self.eval_db(z)
+        f_over_fs = torch.exp(self.eval_a(zs) - self.eval_a(z)) * (1 - z) * (1 + z) * (1 + z**2) \
+            / ((1 - zs) * (1 + zs) * (1 + zs**2))
+        integrand = -4 - 2 * z * dlogg + 4 * zs**4 * f_over_fs / z**4 - 2 * zs**4 * dlogf \
+            * f_over_fs / z**3 + 2 * zs**5 * dlogfs * f_over_fs / z**4 + 2 * zs**4 * dlogg \
+            * f_over_fs / z**3
+        integrand /= (zs**4 * f / (z**4 * fs) - 1)**1.5
+        integrand *= y * g.sqrt()
+        # Extrapolate to y=0
+        y = torch.cat((torch.tensor([0.0], dtype=dreal), y))
+        integrand = torch.cat(
+            (((integrand[1] - integrand[0]) / (y[1] - y[0]) * (-y[0]) + integrand[0]).unsqueeze(-1), integrand))
+        # Add known value for y=1
+        y = torch.cat((y, torch.tensor([1.0], dtype=dreal)))
+        integrand = torch.cat((integrand, torch.tensor([0.0], dtype=dcomplex)))
+        # Integrate
+        dL = torch.trapz(integrand, y)
+        if torch.isnan(dL):
+            logging.error(f'integrate_dL({zs}) = {dL} for a = {self.a} b = {self.b}')
+            logging.error(f'integrand is {integrand}')
+            assert False, 'dL-integrand is nan.'
+        return dL
 
     def integrate_V(self, zs):
         V_c = self.integrate_V_connected(zs)
@@ -76,59 +165,90 @@ class AdSBHNet(nn.Module):
         return V_c - V_d
 
     def integrate_V_connected(self, zs):
-        zs = zs if isinstance(zs, torch.Tensor) else torch.tensor(zs, dtype=dtype)
-        y = torch.linspace(0.001, 0.999, steps=1000, dtype=dtype)
-        z = zs*(1-y)*(1+y)
-        f = self.eval_f(z)
-        g = self.eval_g(z)
-        fs = self.eval_f(zs.unsqueeze(-1))
-        integrand = torch.sqrt(f*g)/((1-y)**2*(1+y)**2)*(1/torch.sqrt(1-(1-y)**4*(1+y)**4*fs/f)-1)*y
+        zs = zs if isinstance(zs, torch.Tensor) else torch.as_tensor(
+            zs, dtype=dcomplex)
+        y = torch.linspace(0.001, 0.999, steps=1000, dtype=dreal)
+        z = zs * (1 - y) * (1 + y)
+        fg = torch.exp(self.eval_b(z) - self.eval_a(z))
+        f_over_fs = torch.exp(self.eval_a(zs) - self.eval_a(z)) * (1 - z) * (1 + z) * (1 + z**2) \
+            / ((1 - zs) * (1 + zs) * (1 + zs**2))
+        integrand = torch.sqrt(fg) / ((1 - y)**2 * (1 + y)**2) * \
+            (1 / torch.sqrt(1 - (1 - y)**4 * (1 + y)**4 / f_over_fs) - 1) * y
         # We extrapolate to y=0
-        y = torch.cat((torch.tensor([0.0], dtype=dtype), y))
-        integrand = torch.cat((((integrand[1]-integrand[0])/(y[1]-y[0])*(-y[0])+integrand[0]).unsqueeze(-1), integrand))
+        y = torch.cat((torch.tensor([0.0], dtype=dreal), y))
+        integrand = torch.cat(
+            (((integrand[1] - integrand[0]) / (y[1] - y[0]) * (-y[0]) + integrand[0]).unsqueeze(-1), integrand))
         # Add analytically known value at y=1
-        y = torch.cat((y, torch.tensor([1.0], dtype=dtype)))
-        integrand = torch.cat((integrand, torch.tensor([0.0], dtype=dtype)))
+        y = torch.cat((y, torch.tensor([1.0], dtype=dreal)))
+        integrand = torch.cat((integrand, torch.tensor([0.0], dtype=dcomplex)))
         # Integrate
-        V = 4*torch.trapz(integrand, y)/zs
+        V = 4 * torch.trapz(integrand, y) / zs
+        if torch.isnan(V):
+            logging.error(f'integrate_V_connected({zs}) = {V} for a = {self.a} b = {self.b}')
+            logging.error(f'integrand is {integrand}')
+            assert False, 'Vc-integrand is nan.'
         return V
 
     def integrate_V_disconnected(self, zs):
-        start = zs.item() if isinstance(zs, torch.Tensor) else zs
-        z = torch.linspace(start, 0.999, steps=1000, dtype=dtype)
-        z[0] = zs
-        f = self.eval_f(z)
-        g = self.eval_g(z)
-        integrand = torch.sqrt(f*g)/z**2
-        # Add analytically known horizon limit
+        # Coordinate is y = (1 - z) / (1 - zs)
+        y = torch.linspace(0.001, 1, steps=1000, dtype=dreal)
+        z = 1 - (1 - zs) * y
+        fg = torch.exp(self.eval_b(z) - self.eval_a(z))
+        integrand = torch.sqrt(fg) / z**2
         # NOTE: This assumes that f(z)*g(z)->1 when z->1
-        z = torch.cat((z, torch.tensor([1.0], dtype=dtype)))
-        integrand = torch.cat((integrand, torch.tensor([1.0], dtype=dtype)))
-        # Integrate
-        V = 2*torch.trapz(integrand, z)
+        y = torch.cat((torch.tensor([0.0], dtype=dreal), y))
+        integrand = torch.cat((torch.tensor([1.0], dtype=dreal), integrand))
+        V = 2 * (1 - zs) * torch.trapz(integrand, y)
+        if torch.isnan(V):
+            logging.error(f'integrate_V_disconnected({zs}) = {V} for a = {self.a} b = {self.b}')
+            logging.error(f'integrand is {integrand}')
+            assert False, 'Vd-integrand is nan.'
         return V
 
     def eval_a(self, z):
         out = torch.zeros_like(z)
         for i, ci in enumerate(self.a):
             for j, cj in enumerate(self.a):
-                out += ci*cj*z**(i+j+1)/(i+j+1)
+                out += ci * cj * z**(i + j + 3) / (i + j + 3)
+        # Normalize
+        return out
+
+    def eval_da(self, z):
+        out = torch.zeros_like(z)
+        for i, ci in enumerate(self.a):
+            for j, cj in enumerate(self.a):
+                out += ci * cj * z**(i + j + 2)
         # Normalize
         return out
 
     def eval_f(self, z):
-        z = z if isinstance(z, torch.Tensor) else torch.from_numpy(z)
-        return (1-z)*(1+z)*(1+z**2)/self.eval_a(z).exp()
+        z = z if isinstance(z, torch.Tensor) else torch.as_tensor(z)
+        return (1 - z) * (1 + z) * (1 + z**2) / self.eval_a(z).exp()
+
+    def eval_df(self, z):
+        return -self.eval_f(z) * self.eval_da(z) - 4 * z**3 / self.eval_a(z).exp()
 
     def eval_b(self, z):
         out = torch.zeros_like(z)
         for i, ci in enumerate(self.b):
-            out += ci*z**(i+1)
-        a1 = self.eval_a(torch.tensor(1.0, dtype=dtype))
+            out += ci * z**(i + 1)
+        a1 = self.eval_a(torch.tensor(1.0, dtype=dreal))
         N = len(self.b)
-        out -= (self.b.sum()+a1)*z**(N+1)
+        out -= (self.b.sum() + a1) * z**(N + 1)
+        return out
+
+    def eval_db(self, z):
+        out = torch.zeros_like(z)
+        for i, ci in enumerate(self.b):
+            out += (i + 1) * ci * z**i
+        a1 = self.eval_a(torch.tensor(1.0, dtype=dreal))
+        N = len(self.b)
+        out -= (N + 1) * (self.b.sum() + a1) * z**N
         return out
 
     def eval_g(self, z):
-        z = z if isinstance(z, torch.Tensor) else torch.from_numpy(z)
-        return self.eval_b(z).exp()/((1-z)*(1+z)*(1+z**2))
+        z = z if isinstance(z, torch.Tensor) else torch.as_tensor(z)
+        return self.eval_b(z).exp() / ((1 - z) * (1 + z) * (1 + z**2))
+
+    def eval_dg(self, z):
+        return self.eval_g(z) * (4 * z**3 / ((1 - z) * (1 + z) * (1 + z**2)) + self.eval_db(z))
